afa535600359f028e909dfe374327639c1974a0c
Rob Moffat <robmoffat@RobMoffatMac.local>
2013-11-06 21:55:56 +0000

    Layout rotation methods

--- core/src/main/java/org/kite9/diagram/position/Layout.java
+++ core/src/main/java/org/kite9/diagram/position/Layout.java
@@ -28,4 +28,43 @@ public enum Layout {
 			return UP;
 		}
 	}
+	
+	public static Layout rotateClockwise(Layout d) {
+		if (d==null)
+			return null;
+		switch (d) {
+		case HORIZONTAL:
+			return VERTICAL;
+		case VERTICAL:
+			return HORIZONTAL;
+		case LEFT: 
+			return UP;
+		case RIGHT:
+			return DOWN;
+		case UP:
+			return RIGHT;
+		default:
+			return LEFT;
+		}
+	}
+	
+	public static Layout rotateAntiClockwise(Layout d) {
+		if (d==null)
+			return null;
+		switch (d) {
+		case HORIZONTAL:
+			return VERTICAL;
+		case VERTICAL:
+			return HORIZONTAL;
+		case LEFT: 
+			return DOWN;
+		case RIGHT:
+			return UP;
+		case UP:
+			return LEFT;
+		default:
+			return RIGHT;
+		}
+	}
 }
+
