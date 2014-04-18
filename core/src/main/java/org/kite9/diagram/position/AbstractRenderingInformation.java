e92a8cc0b724fc33261e970101df1be07fd96117
Rob Moffat <robmoffat@Robs-MacBook.local>
2014-04-18 08:32:10 +0100

    Allowed for transform offsets on the GUI

--- core/src/main/java/org/kite9/diagram/position/AbstractRenderingInformation.java
+++ core/src/main/java/org/kite9/diagram/position/AbstractRenderingInformation.java
@@ -2,13 +2,24 @@ package org.kite9.diagram.position;
 
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
+/**
+ * Contains details of how the diagram element should be shown.  
+ * To speed up the rendering process, path and perimeter should both be normalized to meet 0,0, and
+ * an offset from that point should be given.  
+ * 
+ * This means that when an object moves, we don't need to change the path.
+ * 
+ * @author robmoffat
+ *
+ */
 public abstract class AbstractRenderingInformation implements RenderingInformation {
 
 	private static final long serialVersionUID = 5784432791632841277L;
 	@XStreamAsAttribute
 	protected boolean rendered = true;
-	public String path;
-	public String perimeter;
+	private String path;
+	private String perimeter;
+	private Dimension2D offset;
 
 	public AbstractRenderingInformation() {
 		super();
@@ -20,6 +31,14 @@ public abstract class AbstractRenderingInformation implements RenderingInformati
 
 	public void setRendered(boolean r) {
 		this.rendered = r;
+	}
+	
+	public void setPathOffset(Dimension2D offset) {
+		this.offset = offset;
+	}
+	
+	public Dimension2D getPathOffset() {
+		return this.offset;
 	}
 
 	public void setShapePath(String gp) {
